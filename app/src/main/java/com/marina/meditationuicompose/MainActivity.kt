90ac741a543549dc76ae9f9b7cc464c0513f4d4d
Marina <marisha.jakovleva@gmail.com>
2025-09-28 19:14:45 +0700

    UI

--- app/src/main/java/com/marina/meditationuicompose/MainActivity.kt
+++ app/src/main/java/com/marina/meditationuicompose/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.marina.meditationuicompose.ui.HomeScreen
+import com.marina.meditationuicompose.ui.theme.DeepBlue
 import com.marina.meditationuicompose.ui.theme.MeditationUIComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MeditationUIComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = DeepBlue
+                ) { innerPadding ->
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MeditationUIComposeTheme {
-        Greeting("Android")
     }
 }
